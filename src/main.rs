0f9fe7f075388ddc645c02cd8fe553faa16b585b
Vishal Patil <vishpat@gmail.com>
2023-06-11 12:19:24 -0400

    Added code to return float

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::module::Module;
-use inkwell::types::FloatType;    
+use inkwell::types::FloatType;
 use inkwell::values::FunctionValue;
+
+const MAIN_FUNC_NAME: &str = "main";
 
 pub struct Compiler<'ctx> {
     pub context: &'ctx Context,
     pub builder: Builder<'ctx>,
     pub module: Module<'ctx>,
-    pub f32_type: FloatType<'ctx>, 
+    pub f32_type: FloatType<'ctx>,
     pub main_func: FunctionValue<'ctx>,
-  }
+}
 
+impl<'ctx> Compiler<'ctx> {
+    pub fn new(context: &'ctx Context) -> Self {
+        let builder = context.create_builder();
+        let module = context.create_module(MAIN_FUNC_NAME);
+        let f32_type = context.f32_type();
+        let main_func = module.add_function(MAIN_FUNC_NAME, f32_type.fn_type(&[], false), None);
+        Compiler {
+            context,
+            builder,
+            module,
+            f32_type,
+            main_func,
+        }
+    }
+}
+
+const HG2G: f64 = 42.0;
 fn main() {
-    println!("Hello, world!");
+    let context = Context::create();
+    let compiler = Compiler::new(&context);
+    let main_block =
+    compiler.context.append_basic_block(compiler.main_func, "entry");
+    compiler.builder.position_at_end(main_block);
+    let ret_val = compiler.f32_type.const_float(HG2G);
+    compiler.builder.build_return(Some(&ret_val));
+    compiler.module.print_to_stderr();
 }
